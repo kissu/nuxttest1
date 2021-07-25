12fdee2b8dfd522d25f5b4f2169f19fdc251ce75
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-07-25 22:48:12 +0200

    fix: set a working example

---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-if="user">User name: {{ user.name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    console.log('user ID >>', params.id)
+    const user = await $axios.$get(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    return { user }
+  },
+}
+</script>
