1aa4dbdca6aa7f445881f149a435c3a0d33f3a84
Jérémy VALLES <jeremy@leihia.com>
2021-07-25 20:16:05 +0200

    aa

---
+++ pages/company/_name.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>test</h1>
+    test payload : 
+
+    <p>Path: {{ $route.path }}</p>
+
+    data : {{ company }}
+<!-- <alert>Info alert!</alert>
+    <pre>
+      <code v-highlight="code" class="javascript">
+  export default {
+    modules: [,
+      '@nuxt/content'
+    ],
+    generate: {
+      async ready () {
+        const { $content } = require('@nuxt/content')
+        const files = await $content().only(['slug']).fetch()
+        console.log(files)
+      }
+    }
+  }
+      </code>
+      </pre> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    asyncData({ payload }) {
+      console.log('no payload detected here...')
+      console.log(payload)
+      if (payload) return { company: payload}
+      else {
+        return '-1'
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+      // console.log(this.$router)
+    }
+    // async asyncData({ params }) {
+    //   const slug = await params.slug
+    //   return { slug }
+    // },
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row justify="center" align="center">
+
+<!-- <ul v-for="item in companies" :key="item.id">
+  <li>
+    <NuxtLink :to="`${item.name.toLowerCase()}/`">
+      <li>{{ item.name }}</li>
+    </NuxtLink>
+  </li>
+</ul> -->
+  </v-row>
+</template>
+
+<script>
+// const console = Console
+// import { context } from "vue"
+export default {
+  
+  // async asyncData({ $content }) {
+  //       const companies = await $content('companies')
+  //         .fetch()
+  //     return { companies }
+  //   }
+}
+</script>
